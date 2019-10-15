d9c15db1af08fd8d1e22ebab1e27121439aa23e8
PabloSalazar333 <p.e.salazarmarquez@gmail.com>
2019-10-15 12:16:01 -0500

    FInished a Fibonacci sequence

--- src/com/homework1/app/Factorial.java
+++ src/com/homework1/app/Factorial.java
@@ -2,7 +2,7 @@ package com.homework1.app;
 
 public class Factorial {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Constans
 
 	}
 
--- src/com/homework1/app/FibonacciSequence.java
+++ src/com/homework1/app/FibonacciSequence.java
@@ -1,10 +1,42 @@
 package com.homework1.app;
 
+import java.util.Scanner;
+
 public class FibonacciSequence {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		// Constant declaration
+		
+		//Variable declaration
+		int cont1 = 0;
+		int cont2 = 1;
+		long fibonacci = 1;
+		int bound = 0;
+		
+		Scanner input = new Scanner(System.in);
 
+		do
+		{
+			System.out.print("How many fibonaccis wish calculate: ");
+			bound = input.nextInt();
+			
+			if(bound<=0) 
+			{
+				System.out.println("You must enter a value greater than 0 ");
+			}
+			
+		}while(bound<=0);
+		for(int i=0; i<bound;i++) 
+		{
+			System.out.print(fibonacci+" ");
+			fibonacci = cont1 + cont2;
+			cont1 = cont2;
+			cont2 = (int) fibonacci;
+		}
+		
+		input.close();
+		
 	}
 
 }
